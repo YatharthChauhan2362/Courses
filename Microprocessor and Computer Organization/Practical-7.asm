fe99457f99097788ff3fc2931fe9e401ac7041db
YatharthChauhan2362 <yc232326@gmail.com>
2022-01-09 13:36:34 +0530

    MPCO

--- Microprocessor and Computer Organization/Practical-7.asm
+++ Microprocessor and Computer Organization/Practical-7.asm
@@ -3,3 +3,15 @@
 ; The most significant eight bits of the two numbers are in memory locations 4001H and 4003H. Store the result in memory locations 
 ; 4004H and 4005H with the most significant byte in Memory location 4005H. 
 
+ORG 100H
+
+MOV [4002H],5050H
+MOV [4000H],2020H
+
+MOV AX,[4000H]
+MOV BX,[4002H]
+
+SUB AX,BX
+MOV [4004H],AX
+
+RET

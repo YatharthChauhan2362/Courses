4a5a5d03d710531db1a921908cb249909d1e6486
YatharthChauhan2362 <yc232326@gmail.com>
2022-04-06 17:25:01 +0530

    MPCO

--- Microprocessor and Computer Organization/MPCO Practicals/Practical-32.asm
+++ Microprocessor and Computer Organization/MPCO Practicals/Practical-32.asm
@@ -2,16 +2,15 @@
 ; You may customize this and other start-up templates; 
 ; The location of this template is c:\emu8086\inc\0_com_template.txt
 
-org 100h   
+org 100h
 
-MOV AX,7Ah
-MOV BX,46H
-ADD AX,BX
-MOV [2098H],AX
-PUSHF
-POP [2097H]    
+MOV BL, 50H
+MOV BH, 50H
+XOR BH, BL
+NOT BH
 
 ret
+
 
 
 
